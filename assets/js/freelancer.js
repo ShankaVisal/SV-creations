c033c74db453b372e2273e10c4b96040a1298445
Shanka Visal <shankavisal@gmail.com>
2023-02-06 19:31:38 +0530

    add file

--- assets/js/freelancer.js
+++ assets/js/freelancer.js
@@ -5,7 +5,14 @@
   
   if (scrollToTop) {
     
-    // Scroll to top button appear
+    // Scroll to top <div class="row">
+    <div class="col-lg-4 ms-auto">
+        <p class="lead"><br /><span style="color: rgb(0, 0, 0);">We are a design solutions company with 2 years of experience. We believe that creative vision can create a beautiful world. We have a great team with a creative mindset to design what you need.  </span><br /><br /><span style="color: rgb(0, 0, 0);">We provide our service through fiver freelancer and many online platforms.</span><br /><br /></p>
+    </div>
+    <div class="col-lg-4 me-auto">
+        <p class="lead"><br /><span style="color: rgb(0, 0, 0); background-color: rgba(24, 188, 156, 0);">We understand your brand&#39;s purpose, vision, values, audience, competition, personality, etc. through in-depth discovery sessions to establish an appropriate positioning strategy.</span><br /><br /><span style="color: rgb(0, 0, 0); background-color: rgba(24, 188, 156, 0);">We give your brand an authentic and iconic identity with a thoughtful name, a timeless logo, inviting product designs, and an extendible visual language.</span><br /><br /></p>
+    </div>
+</div>utton appear
     window.addEventListener('scroll', function() {
       var scrollDistance = window.pageYOffset;
 
